12bb852b43be6c7b2e6a1ed7af061f8d5045c224
eulerfcosta <euler.dacosta@gmail.com>
2020-03-30 17:51:56 -0300

    c001 - Estutura inicial para projeto angular 9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './modules/login/login.component';
+import { AdminComponent } from './modules/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '' , component: LoginComponent},
+    {path: 'admin' , component: AdminComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
