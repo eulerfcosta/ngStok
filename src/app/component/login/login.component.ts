a50fbe2dca63c17d548fd47b7608a27ccd1d3f0a
eulerfcosta <euler.dacosta@gmail.com>
2024-02-22 00:42:24 -0300

    clean unsed imports

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -2,8 +2,6 @@ import {Component} from '@angular/core';
 import {MatCard, MatCardActions, MatCardContent, MatCardFooter, MatCardHeader, MatCardTitle} from '@angular/material/card';
 import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
 import {MatIcon} from '@angular/material/icon';
 import {MatButton} from '@angular/material/button';
 
@@ -18,7 +16,6 @@ import {MatButton} from '@angular/material/button';
         MatInput,
         MatLabel,
         MatError,
-        ReactiveFormsModule,
         MatCardContent,
         MatIcon,
         MatCardFooter,
