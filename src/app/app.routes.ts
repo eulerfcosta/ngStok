8b85a42ae56363ab7b234fb5d7ad667c889c8528
eulerfcosta <euler.dacosta@gmail.com>
2024-03-07 11:04:20 -0300

    modal para exibir mensagens de erro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from './component/login/login.component';
 import {RecoverPasswordComponent} from './component/recover-password/recover-password.component';
 import {BaseComponent} from './component/application/base/base.component';
+import {DashboardComponent} from './application/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
     {path: 'login', component: LoginComponent},
     {path: 'forgot-password', component: RecoverPasswordComponent},
-    {path:'appllication', component: BaseComponent}
+    {path: 'appllication', component: BaseComponent, children: [{path: 'dashboard', component: DashboardComponent}]},
+
 ];
